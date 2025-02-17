6853a64bd72926a89ddffba584deba3917cc3441
Victor <viictorzgz@gmail.com>
2025-02-17 18:01:47 +0100

    Arreglo de sass con vue

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="header">
+        <div class="header__logo"><img src="../assets/images/LogoCinezar.png" alt="LogoCine"></div>
+        <h1 class="header__title" onclick="window.location.href='../html/index.html'">CineZar</h1>
+
+    </header>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'Header',
+});
+</script>
+
+<style lang="scss">
+.header {
+    background-color: $secondary-color
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+// import './assets/styles/main.scss';
 
 const app = createApp(App)
 

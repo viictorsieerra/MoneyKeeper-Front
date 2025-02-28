a04b5904cd5f7d7783a5086bbbd6a7ac66de8415
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-02-28 18:23:54 +0100

    solucionado error merge (Alfonso)

--- src/router/index.ts
+++ src/router/index.ts
@@ -33,14 +33,7 @@ const router = createRouter({
       path: '/zona-privada',
       component: PrivateHomeView
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+   ,
   ],
 })
 

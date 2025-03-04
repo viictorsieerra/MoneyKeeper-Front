a3c76e364be34062abfac8aca8a7f18eae859f8f
Victor <viictorzgz@gmail.com>
2025-03-04 19:37:22 +0100

    Registro de usuario aplicado

--- src/stores/JWT.ts
+++ src/stores/JWT.ts
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import LoginDTO from './DTO/LoginDTO'
+import RegistroDTO from './DTO/RegistroDTO'
 
 export const useJWTStore = defineStore('jwt', () => {
   var jwt = ref('')
@@ -26,5 +27,30 @@ export const useJWTStore = defineStore('jwt', () => {
       .catch(error => console.log(error))
   }
 
-  return { jwt, loginUser }
+  function registerUser(registroUser: RegistroDTO) {
+    fetch("https://localhost:7053/JWT/Register", {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(
+        {
+          _idUsuario: 0,
+          _nombre: registroUser._nombre,
+          _apellido: registroUser._apellido,
+          _correo: registroUser._correo,
+          _contrasena: registroUser._contrasena,
+          _dni: registroUser._dni,
+          _fecNacimiento: registroUser._fecNacimiento
+        }
+      )
+    })
+      .then(res => res.text())
+      .then(data => {
+        console.log(data)
+        jwt.value = data
+        console.log(`Token: ${jwt}`)
+      })
+      .catch(error => console.log(error))
+  }
+
+  return { jwt, loginUser, registerUser }
 }, {persist: true})

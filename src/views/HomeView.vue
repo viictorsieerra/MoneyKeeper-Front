168b22005921a6bed459258ccd5e136208ba21be
COroALfonso <a27803@svalero.com>
2025-03-05 19:20:38 +0100

    eliminado apartado de la home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
 import Hero from '../components/Hero.vue';
 import Features from '../components/Features.vue';
-import Comentarios from '../components/Comentarios.vue';
-import Opiniones from '../components/Opiniones.vue'
+
 import Registro from '@/components/Registro.vue';
 </script>
 
 <template>
     <Features />
     <Hero />
-    <Comentarios />
-    <Opiniones />
+    
     <Registro />
 </template>
